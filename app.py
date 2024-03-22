50771e656c9b21e0c8b6c53b15fafc9607ccbc23
pircher <jordan.pircher@arrk-engineering.com>
2024-03-22 16:22:50 +0100

    add client, embeddings, models

--- app.py
+++ app.py
@@ -12,5 +12,47 @@ from langchain.llms.bedrock import Bedrock
 
 import numpy as np
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 
+## Vector Embedding And Vectore Store
+
+from langchain.vectorstores import FAISS
+
+## LLM Models
+
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+# Bedrock Clients
+bedrock = boto3.client(service_name="bedrock-runtime")
+bedrock_embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client=bedrock)
+
+
+## Data ingestion
+
+def data_ingestion():
+    loader = PyPDFDirectoryLoader("data")
+    documents = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chuck_size=10000, chunk_overlap=1000)
+    docs = text_splitter.split_documents(documents)
+    return docs
+
+
+## Vector Embedding and vector store
+
+def get_vector_store(docs):
+    vectorstore_faiss = FAISS.from_documents(docs, bedrock_embeddings)
+    vectorstore_faiss.save_local("faiss_index")
+
+
+def get_claude_llm():
+    ## create the Anthropic Model
+    llm = Bedrock(model_id="anthropic.claude-v2:1", client=bedrock, model_kwargs={'maxTokens': 512})
+    return llm
+
+
+def get_llama2_llm():
+    ## create the Anthropic Model
+    llm = Bedrock(model_id="meta.llama2-13b-chat-v1", client=bedrock, model_kwargs={'max_gen_len': 512})
+    return llm
